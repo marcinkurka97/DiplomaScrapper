50f74fd4f71dbd25b1bda1d00cc058dad0664f9e
Marcin <marcinkurka97@gmail.com>
2019-11-02 23:36:04 +0100

    Created local DB using lowdb, CRON scraping every 30 mins

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-import { getOffersObj } from "./lib/scrapper";
 import express from "express";
+import { getOffersObj } from "./lib/scrapper";
+import db from "./lib/db";
+import "./lib/cron";
 
+// Setup Express Server
 const app = express();
+
 app.get("/scrape", async (req, res, next) => {
   console.log("Scraping!");
   const offersPromise = await getOffersObj();
